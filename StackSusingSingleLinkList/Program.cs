6b37b0e293be3695717dbf6c8e1def2df13ac8fa
Aditya Hari <aditya.hari.ft21@mail.umy.ac.id>
2022-11-28 17:06:31 +0700

    add display method and main program

--- StackSusingSingleLinkList/Program.cs
+++ StackSusingSingleLinkList/Program.cs
@@ -42,11 +42,62 @@ namespace StackSusingSingleLinkList
 
         public void pop()
         {
-           
+            Console.WriteLine("\n The poped element is: " + top.info);
+            top = top.next;
+        }
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack is Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+            }
         }
         static void Main(string[] args)
         {
-            
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\nStack Menu");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice: ");
+                string Input = Console.ReadLine();
+                char ch = Convert.ToChar(Input == "" ? "o" : Input);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a Number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack is Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
+
+            }
         }
     }
     
